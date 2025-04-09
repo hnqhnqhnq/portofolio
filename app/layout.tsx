9a1d32098a7914ba630a9e1f5bd9b758e66cf540
Hnq <hancu345@gmail.com>
2025-04-09 17:00:44 +0300

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,39 @@
-import type React from "react"
-import "./globals.css"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
-import Navbar from "@/components/navbar"
+import type React from "react";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/navbar";
 
 const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-})
+   subsets: ["latin"],
+   variable: "--font-inter",
+});
 
 export const metadata = {
-  title: "Hîncu Ștefan | Backend Developer",
-  description: "Personal portfolio of Hîncu Ștefan, a backend developer specializing in Node.js, Express, and MongoDB.",
-    generator: 'v0.dev'
-}
+   title: "Hîncu Ștefan | Backend Developer",
+   description:
+      "Personal portfolio of Hîncu Ștefan, a backend developer specializing in Node.js, Express, and MongoDB.",
+};
 
 export default function RootLayout({
-  children,
+   children,
 }: {
-  children: React.ReactNode
+   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" suppressHydrationWarning className={`${inter.variable} scroll-smooth`}>
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-          <Navbar />
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  )
+   return (
+      <html
+         lang='en'
+         suppressHydrationWarning
+         className={`${inter.variable} scroll-smooth`}
+      >
+         <body className={inter.className}>
+            <ThemeProvider attribute='class' defaultTheme='light' enableSystem>
+               <Navbar />
+               {children}
+            </ThemeProvider>
+         </body>
+      </html>
+   );
 }
 
-
-
-import './globals.css'
+import "./globals.css";
